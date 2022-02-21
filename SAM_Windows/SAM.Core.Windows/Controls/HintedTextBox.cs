4cd441b62e9f2df6ae0d92ab3ddbf6259c3d29cd
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-02-21 09:07:13 +0100

    HintedTextBox Control and ControlText_NumberOnly EventHandler Added

---
+++ SAM_Windows/SAM.Core.Windows/Controls/HintedTextBox.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace SAM.Core.Windows
+{
+    public class HintedTextBox : System.Windows.Forms.TextBox
+    {
+        private System.Drawing.Color defaultColor;
+        private string hint;
+
+        public string Hint
+        {
+            get
+            {
+                return hint;
+            }
+
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    // change style   
+                    ForeColor = System.Drawing.Color.Gray;
+                    // Add text
+                    hint = value;
+                    Text = value;
+                }
+            }
+        }
+        public HintedTextBox()
+        {
+            // get default color of text
+            defaultColor = ForeColor;
+            // Add event handler for when the control gets focus
+            GotFocus += (object sender, EventArgs e) =>
+            {
+                Text = string.Empty;
+                ForeColor = defaultColor;
+            };
+
+            // add event handling when focus is lost
+            LostFocus += (object sender, EventArgs e) => {
+                if (string.IsNullOrEmpty(Text) || Text == Hint)
+                {
+                    ForeColor = System.Drawing.Color.Gray;
+                    Text = Hint;
+                }
+                else
+                {
+                    ForeColor = defaultColor;
+                }
+            };
+        }
+    }
+}
+
---
+++ SAM_Windows/SAM.Core.Windows/EventHandler/ControlText_NumberOnly.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Windows.Forms;
+
+namespace SAM.Core.Windows
+{
+    public static partial class EventHandler
+    {
+        public static void ControlText_NumberOnly(object sender, KeyPressEventArgs e)
+        {
+            string separator_String = CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+            char separator = '.';
+            if (!string.IsNullOrWhiteSpace(separator_String))
+                separator = separator_String.Trim()[0];
+
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != separator))
+                e.Handled = true;
+
+            // only allow one decimal point
+            string text = (sender as Control)?.Text;
+            if ((e.KeyChar == separator) && !string.IsNullOrWhiteSpace(text) && (text.IndexOf(separator) > -1))
+                e.Handled = true;
+        }
+
+    }
+}
