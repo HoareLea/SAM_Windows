ce1672d83581ad332612fb1ade25e83362f70f87
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-03-11 11:54:02 +0000

    SearchForm Implemented

---
+++ SAM_Windows/SAM.Core.Windows/Forms/SearchForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace SAM.Core.Windows.Forms
+{
+    partial class SearchForm<T>
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox_Text = new System.Windows.Forms.TextBox();
+            this.ListBox_Texts = new System.Windows.Forms.ListBox();
+            this.Button_OK = new System.Windows.Forms.Button();
+            this.Button_Cancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextBox_Text
+            // 
+            this.TextBox_Text.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBox_Text.Location = new System.Drawing.Point(12, 12);
+            this.TextBox_Text.Name = "TextBox_Text";
+            this.TextBox_Text.Size = new System.Drawing.Size(208, 20);
+            this.TextBox_Text.TabIndex = 0;
+            this.TextBox_Text.TextChanged += new System.EventHandler(this.TextBox_Text_TextChanged);
+            // 
+            // ListBox_Texts
+            // 
+            this.ListBox_Texts.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ListBox_Texts.FormattingEnabled = true;
+            this.ListBox_Texts.Location = new System.Drawing.Point(12, 40);
+            this.ListBox_Texts.Name = "ListBox_Texts";
+            this.ListBox_Texts.Size = new System.Drawing.Size(208, 251);
+            this.ListBox_Texts.TabIndex = 1;
+            // 
+            // Button_OK
+            // 
+            this.Button_OK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Button_OK.Location = new System.Drawing.Point(64, 313);
+            this.Button_OK.Name = "Button_OK";
+            this.Button_OK.Size = new System.Drawing.Size(75, 28);
+            this.Button_OK.TabIndex = 6;
+            this.Button_OK.Text = "OK";
+            this.Button_OK.UseVisualStyleBackColor = true;
+            this.Button_OK.Click += new System.EventHandler(this.Button_OK_Click);
+            // 
+            // Button_Cancel
+            // 
+            this.Button_Cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Button_Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Button_Cancel.Location = new System.Drawing.Point(145, 313);
+            this.Button_Cancel.Name = "Button_Cancel";
+            this.Button_Cancel.Size = new System.Drawing.Size(75, 28);
+            this.Button_Cancel.TabIndex = 5;
+            this.Button_Cancel.Text = "Cancel";
+            this.Button_Cancel.UseVisualStyleBackColor = true;
+            this.Button_Cancel.Click += new System.EventHandler(this.Button_Cancel_Click);
+            // 
+            // SearchForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(232, 353);
+            this.Controls.Add(this.Button_OK);
+            this.Controls.Add(this.Button_Cancel);
+            this.Controls.Add(this.ListBox_Texts);
+            this.Controls.Add(this.TextBox_Text);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(250, 400);
+            this.Name = "SearchForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "SearchForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox_Text;
+        private System.Windows.Forms.ListBox ListBox_Texts;
+        private System.Windows.Forms.Button Button_OK;
+        private System.Windows.Forms.Button Button_Cancel;
+    }
+}
---
+++ SAM_Windows/SAM.Core.Windows/Forms/SearchForm.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SAM.Core.Windows.Forms
+{
+    public partial class SearchForm<T> : Form
+    {
+        private SearchWrapper searchWrapper;
+
+        private Dictionary<string, T> dictionary;
+        
+        public SearchForm()
+        {
+            InitializeComponent();
+        }
+
+        public SearchForm(string name, IEnumerable<T> items, Func<T, string> text, bool caseSensitive = false)
+        {
+            InitializeComponent();
+
+            Text = name;
+
+            searchWrapper = new SearchWrapper(caseSensitive);
+            if(items != null)
+            {
+                dictionary = new Dictionary<string, T>();
+                foreach(T item in items)
+                {
+                    string value = text == null ? item.ToString() : text.Invoke(item);
+
+                    if(searchWrapper.Add(value))
+                    {
+                        dictionary[value] = item;
+                    }
+                }
+            }
+
+            Search();
+        }
+
+        private void Search()
+        {
+
+            List<string> selectedTexts = null;
+            if(ListBox_Texts.SelectedItems != null && ListBox_Texts.SelectedItems.Count != 0)
+            {
+                selectedTexts = new List<string>();
+                foreach (string selectedText in ListBox_Texts.SelectedItems)
+                {
+                    selectedTexts.Add(selectedText);
+                }
+            }
+
+            ListBox_Texts.Items.Clear();
+
+            if (searchWrapper == null)
+            {
+                return;
+            }
+
+            List<string> texts = null; ;
+            if (string.IsNullOrEmpty(TextBox_Text.Text) || TextBox_Text.Text.Length < 3)
+            {
+                IEnumerable<string> texts_Temp = searchWrapper.Texts;
+                if (texts_Temp != null)
+                {
+                    texts = new List<string>(texts_Temp);
+                }
+            }
+            else
+            {
+                texts = searchWrapper.Search(TextBox_Text.Text, true);
+            }
+
+            if (texts == null || texts.Count == 0)
+            {
+                return;
+            }
+
+            foreach (string text in texts)
+            {
+                ListBox_Texts.Items.Add(text);
+            }
+
+            if (selectedTexts != null && selectedTexts.Count != 0)
+            {
+                foreach (string selectedText in selectedTexts)
+                {
+                    ListBox_Texts.SelectedItems.Add(selectedText);
+                }
+            }
+        }
+
+        private void Button_Cancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        private void Button_OK_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void TextBox_Text_TextChanged(object sender, EventArgs e)
+        {
+            Search();
+        }
+
+
+        public string SearchText
+        {
+            get
+            {
+                return TextBox_Text.Text;
+            }
+            set
+            {
+                TextBox_Text.Text = value;
+                TextBox_Text.SelectAll();
+            }
+        }
+
+        public SelectionMode SelectionMode
+        {
+            get
+            {
+                return ListBox_Texts.SelectionMode;
+            }
+
+            set
+            {
+                ListBox_Texts.SelectionMode = value;
+            }
+        }
+
+        public List<T> SelectedItems
+        {
+            get
+            {
+                if(ListBox_Texts.SelectedItems == null)
+                {
+                    return null;
+                }
+
+                List<T> result = new List<T>();
+                foreach(string text in ListBox_Texts.SelectedItems)
+                {
+                    if(dictionary.TryGetValue(text, out T item))
+                    {
+                        result.Add(item);
+                    }
+                }
+
+                return result;
+            }
+        }
+    }
+}
