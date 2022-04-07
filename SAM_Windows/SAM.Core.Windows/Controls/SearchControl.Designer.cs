1b7d9afc92d10c541e4b23358780c72bb1a28d58
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-04-07 15:09:56 +0100

    SearchControl Added

---
+++ SAM_Windows/SAM.Core.Windows/Controls/SearchControl.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace SAM.Core.Windows
+{
+    partial class SearchControl<T>
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListBox_Texts = new System.Windows.Forms.ListBox();
+            this.TextBox_Text = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ListBox_Texts
+            // 
+            this.ListBox_Texts.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ListBox_Texts.FormattingEnabled = true;
+            this.ListBox_Texts.ItemHeight = 16;
+            this.ListBox_Texts.Location = new System.Drawing.Point(3, 31);
+            this.ListBox_Texts.Name = "ListBox_Texts";
+            this.ListBox_Texts.Size = new System.Drawing.Size(194, 260);
+            this.ListBox_Texts.TabIndex = 3;
+            this.ListBox_Texts.SelectedIndexChanged += new System.EventHandler(this.ListBox_Texts_SelectedIndexChanged);
+            this.ListBox_Texts.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.ListBox_Texts_MouseDoubleClick);
+            // 
+            // TextBox_Text
+            // 
+            this.TextBox_Text.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBox_Text.Location = new System.Drawing.Point(3, 3);
+            this.TextBox_Text.Name = "TextBox_Text";
+            this.TextBox_Text.Size = new System.Drawing.Size(194, 22);
+            this.TextBox_Text.TabIndex = 2;
+            this.TextBox_Text.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox_Text_KeyDown);
+            // 
+            // SearchControl
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.Controls.Add(this.ListBox_Texts);
+            this.Controls.Add(this.TextBox_Text);
+            this.Name = "SearchControl";
+            this.Size = new System.Drawing.Size(200, 300);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListBox_Texts;
+        private System.Windows.Forms.TextBox TextBox_Text;
+    }
+}
---
+++ SAM_Windows/SAM.Core.Windows/Controls/SearchControl.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace SAM.Core.Windows
+{
+    public partial class SearchControl<T> : UserControl
+    {
+        private SearchWrapper searchWrapper;
+
+        private Dictionary<string, T> dictionary;
+
+        public MouseEventHandler MouseDoubleClick;
+
+        public System.EventHandler SelectedIndexChanged;
+
+        public SearchControl()
+        {
+            InitializeComponent();
+        }
+
+        public SearchControl(IEnumerable<T> items, Func<T, string> text, bool caseSensitive = false)
+        {
+            InitializeComponent();
+
+            searchWrapper = new SearchWrapper(caseSensitive);
+            if (items != null)
+            {
+                dictionary = new Dictionary<string, T>();
+                foreach (T item in items)
+                {
+                    string value = text == null ? item.ToString() : text.Invoke(item);
+
+                    if (searchWrapper.Add(value))
+                    {
+                        dictionary[value] = item;
+                    }
+                }
+            }
+
+            Search();
+        }
+
+        private void Search()
+        {
+
+            List<string> selectedTexts = null;
+            if (ListBox_Texts.SelectedItems != null && ListBox_Texts.SelectedItems.Count != 0)
+            {
+                selectedTexts = new List<string>();
+                foreach (string selectedText in ListBox_Texts.SelectedItems)
+                {
+                    selectedTexts.Add(selectedText);
+                }
+            }
+
+            ListBox_Texts.Items.Clear();
+
+            if (searchWrapper == null)
+            {
+                return;
+            }
+
+            List<string> texts = null; ;
+            if (string.IsNullOrEmpty(TextBox_Text.Text) || TextBox_Text.Text.Length < 3)
+            {
+                IEnumerable<string> texts_Temp = searchWrapper.Texts;
+                if (texts_Temp != null)
+                {
+                    texts = new List<string>(texts_Temp);
+                }
+            }
+            else
+            {
+                texts = searchWrapper.Search(TextBox_Text.Text, true);
+            }
+
+            if (texts == null || texts.Count == 0)
+            {
+                return;
+            }
+
+            foreach (string text in texts)
+            {
+                ListBox_Texts.Items.Add(text);
+            }
+
+            if (selectedTexts != null && selectedTexts.Count != 0)
+            {
+                foreach (string selectedText in selectedTexts)
+                {
+                    ListBox_Texts.SelectedItems.Add(selectedText);
+                }
+            }
+        }
+
+        public string SearchText
+        {
+            get
+            {
+                return TextBox_Text.Text;
+            }
+            set
+            {
+                TextBox_Text.Text = value;
+                TextBox_Text.SelectAll();
+            }
+        }
+
+        public SelectionMode SelectionMode
+        {
+            get
+            {
+                return ListBox_Texts.SelectionMode;
+            }
+
+            set
+            {
+                ListBox_Texts.SelectionMode = value;
+            }
+        }
+
+        public List<T> SelectedItems
+        {
+            get
+            {
+                if (ListBox_Texts.SelectedItems == null)
+                {
+                    return null;
+                }
+
+                List<T> result = new List<T>();
+                foreach (string text in ListBox_Texts.SelectedItems)
+                {
+                    if (dictionary.TryGetValue(text, out T item))
+                    {
+                        result.Add(item);
+                    }
+                }
+
+                return result;
+            }
+        }
+
+        private void ListBox_Texts_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            MouseEventHandler mouseEventHandler = MouseDoubleClick;
+            if (mouseEventHandler != null)
+            {
+                mouseEventHandler(this, e);
+            }
+        }
+
+        private void TextBox_Text_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down && ListBox_Texts.Items != null && ListBox_Texts.Items.Count > 0)
+            {
+                ListBox_Texts.Focus();
+            }
+        }
+
+        private void ListBox_Texts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            System.EventHandler eventHandler = SelectedIndexChanged;
+            if (eventHandler != null)
+            {
+                eventHandler(this, e);
+            }
+        }
+    }
+}
