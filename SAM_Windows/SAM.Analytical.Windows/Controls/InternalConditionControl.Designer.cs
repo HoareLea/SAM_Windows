084910bcc3201720940001fd132613ec00f9c412
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-04-08 15:28:43 +0100

    InternalConditionControl Initial Implementation

---
+++ SAM_Windows/SAM.Analytical.Windows/Controls/InternalConditionControl.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace SAM.Analytical.Windows
+{
+    partial class InternalConditionControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label_Name = new System.Windows.Forms.Label();
+            this.TextBox_Name = new System.Windows.Forms.TextBox();
+            this.PropertyGrid_Main = new System.Windows.Forms.PropertyGrid();
+            this.SuspendLayout();
+            // 
+            // Label_Name
+            // 
+            this.Label_Name.AutoSize = true;
+            this.Label_Name.Location = new System.Drawing.Point(3, 9);
+            this.Label_Name.Name = "Label_Name";
+            this.Label_Name.Size = new System.Drawing.Size(49, 17);
+            this.Label_Name.TabIndex = 0;
+            this.Label_Name.Text = "Name:";
+            // 
+            // TextBox_Name
+            // 
+            this.TextBox_Name.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBox_Name.Location = new System.Drawing.Point(58, 6);
+            this.TextBox_Name.Name = "TextBox_Name";
+            this.TextBox_Name.Size = new System.Drawing.Size(270, 22);
+            this.TextBox_Name.TabIndex = 1;
+            // 
+            // PropertyGrid_Main
+            // 
+            this.PropertyGrid_Main.Location = new System.Drawing.Point(6, 34);
+            this.PropertyGrid_Main.Name = "PropertyGrid_Main";
+            this.PropertyGrid_Main.Size = new System.Drawing.Size(322, 359);
+            this.PropertyGrid_Main.TabIndex = 2;
+            // 
+            // InternalConditionControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.PropertyGrid_Main);
+            this.Controls.Add(this.TextBox_Name);
+            this.Controls.Add(this.Label_Name);
+            this.Name = "InternalConditionControl";
+            this.Size = new System.Drawing.Size(331, 396);
+            this.Load += new System.EventHandler(this.InternalConditionControl_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label_Name;
+        private System.Windows.Forms.TextBox TextBox_Name;
+        private System.Windows.Forms.PropertyGrid PropertyGrid_Main;
+    }
+}
---
+++ SAM_Windows/SAM.Analytical.Windows/Controls/InternalConditionControl.cs
@@ -0,0 +1,114 @@
+﻿using SAM.Core.Windows;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SAM.Analytical.Windows
+{
+    public partial class InternalConditionControl : UserControl
+    {
+        private ProfileLibrary profileLibrary;
+
+        private InternalCondition internalCondition;
+        private HashSet<Enum> enums;
+
+        public InternalConditionControl()
+        {
+            InitializeComponent();
+        }
+
+        public InternalConditionControl(InternalCondition internalCondition, ProfileLibrary profileLibrary = null, IEnumerable<Enum> enums = null)
+        {
+            this.internalCondition = internalCondition;
+            this.profileLibrary = profileLibrary;
+
+            if(enums != null)
+            {
+                this.enums = new HashSet<Enum>();
+                foreach(Enum @enum in enums)
+                {
+                    this.enums.Add(@enum);
+                }
+            }
+        }
+
+        private void InternalConditionControl_Load(object sender, EventArgs e)
+        {
+            LoadInternalCondition();
+        }
+
+        [Browsable(false)]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public InternalCondition InternalCondition
+        {
+            get
+            {
+                return internalCondition;
+            }
+            set
+            {
+                internalCondition = value;
+                LoadInternalCondition();
+            }
+        }
+
+        [Browsable(false)]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public ProfileLibrary ProfileLibrary
+        {
+            get
+            {
+                return profileLibrary;
+            }
+            set
+            {
+                profileLibrary = value;
+            }
+        }
+
+        public List<Enum> Enums
+        {
+            get
+            {
+                return enums?.ToList();
+            }
+            set
+            {
+                enums = null;
+
+                if (value != null)
+                {
+                    enums = new HashSet<Enum>();
+                    foreach (Enum @enum in value)
+                    {
+                        enums.Add(@enum);
+                    }
+                }
+
+                LoadParameters();
+            }
+        }
+
+        private void LoadParameters()
+        {
+            PropertyGrid_Main.SelectedObject = null;
+
+            CustomParameters customParameters = Core.Windows.Create.CustomParameters(internalCondition, enums?.ToArray());
+
+            PropertyGrid_Main.SelectedObject = customParameters;
+        }
+
+        private void LoadInternalCondition()
+        {
+            TextBox_Name.Text = internalCondition?.Name;
+
+            LoadParameters();
+        }
+
+
+    }
+}
