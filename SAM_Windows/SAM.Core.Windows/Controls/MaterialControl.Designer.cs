b04fc8e8bcd4b151b5845a7f50358f0daf6329a7
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-04-08 09:40:03 +0100

    MaterialControl Added

---
+++ SAM_Windows/SAM.Core.Windows/Controls/MaterialControl.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace SAM.Core.Windows
+{
+    partial class MaterialControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox_Density = new System.Windows.Forms.TextBox();
+            this.Label_Density = new System.Windows.Forms.Label();
+            this.TextBox_SpecificHeatCapacity = new System.Windows.Forms.TextBox();
+            this.Label_SpecificHeatCapacity = new System.Windows.Forms.Label();
+            this.TextBox_ThermalConductivity = new System.Windows.Forms.TextBox();
+            this.Label_ThermalConductivity = new System.Windows.Forms.Label();
+            this.TextBox_Description = new System.Windows.Forms.TextBox();
+            this.Label_Description = new System.Windows.Forms.Label();
+            this.TextBox_DisplayName = new System.Windows.Forms.TextBox();
+            this.Label_DisplayName = new System.Windows.Forms.Label();
+            this.TextBox_Name = new System.Windows.Forms.TextBox();
+            this.Label_Name = new System.Windows.Forms.Label();
+            this.PropertyGrid_Parameters = new System.Windows.Forms.PropertyGrid();
+            this.SuspendLayout();
+            // 
+            // TextBox_Density
+            // 
+            this.TextBox_Density.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBox_Density.Location = new System.Drawing.Point(209, 143);
+            this.TextBox_Density.Name = "TextBox_Density";
+            this.TextBox_Density.Size = new System.Drawing.Size(138, 22);
+            this.TextBox_Density.TabIndex = 23;
+            // 
+            // Label_Density
+            // 
+            this.Label_Density.AutoSize = true;
+            this.Label_Density.Location = new System.Drawing.Point(3, 146);
+            this.Label_Density.Name = "Label_Density";
+            this.Label_Density.Size = new System.Drawing.Size(109, 17);
+            this.Label_Density.TabIndex = 20;
+            this.Label_Density.Text = "Density [kg/m3]:";
+            // 
+            // TextBox_SpecificHeatCapacity
+            // 
+            this.TextBox_SpecificHeatCapacity.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBox_SpecificHeatCapacity.Location = new System.Drawing.Point(209, 115);
+            this.TextBox_SpecificHeatCapacity.Name = "TextBox_SpecificHeatCapacity";
+            this.TextBox_SpecificHeatCapacity.Size = new System.Drawing.Size(138, 22);
+            this.TextBox_SpecificHeatCapacity.TabIndex = 24;
+            // 
+            // Label_SpecificHeatCapacity
+            // 
+            this.Label_SpecificHeatCapacity.AutoSize = true;
+            this.Label_SpecificHeatCapacity.Location = new System.Drawing.Point(3, 118);
+            this.Label_SpecificHeatCapacity.Name = "Label_SpecificHeatCapacity";
+            this.Label_SpecificHeatCapacity.Size = new System.Drawing.Size(200, 17);
+            this.Label_SpecificHeatCapacity.TabIndex = 21;
+            this.Label_SpecificHeatCapacity.Text = "Specific Heat Capacity [J/kgK]:";
+            // 
+            // TextBox_ThermalConductivity
+            // 
+            this.TextBox_ThermalConductivity.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBox_ThermalConductivity.Location = new System.Drawing.Point(209, 87);
+            this.TextBox_ThermalConductivity.Name = "TextBox_ThermalConductivity";
+            this.TextBox_ThermalConductivity.Size = new System.Drawing.Size(138, 22);
+            this.TextBox_ThermalConductivity.TabIndex = 25;
+            // 
+            // Label_ThermalConductivity
+            // 
+            this.Label_ThermalConductivity.AutoSize = true;
+            this.Label_ThermalConductivity.Location = new System.Drawing.Point(3, 90);
+            this.Label_ThermalConductivity.Name = "Label_ThermalConductivity";
+            this.Label_ThermalConductivity.Size = new System.Drawing.Size(193, 17);
+            this.Label_ThermalConductivity.TabIndex = 22;
+            this.Label_ThermalConductivity.Text = "Thermal Conductivity [W/mK]:";
+            // 
+            // TextBox_Description
+            // 
+            this.TextBox_Description.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBox_Description.Location = new System.Drawing.Point(108, 59);
+            this.TextBox_Description.Name = "TextBox_Description";
+            this.TextBox_Description.Size = new System.Drawing.Size(239, 22);
+            this.TextBox_Description.TabIndex = 19;
+            // 
+            // Label_Description
+            // 
+            this.Label_Description.AutoSize = true;
+            this.Label_Description.Location = new System.Drawing.Point(3, 62);
+            this.Label_Description.Name = "Label_Description";
+            this.Label_Description.Size = new System.Drawing.Size(83, 17);
+            this.Label_Description.TabIndex = 18;
+            this.Label_Description.Text = "Description:";
+            // 
+            // TextBox_DisplayName
+            // 
+            this.TextBox_DisplayName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBox_DisplayName.Location = new System.Drawing.Point(108, 31);
+            this.TextBox_DisplayName.Name = "TextBox_DisplayName";
+            this.TextBox_DisplayName.Size = new System.Drawing.Size(239, 22);
+            this.TextBox_DisplayName.TabIndex = 17;
+            // 
+            // Label_DisplayName
+            // 
+            this.Label_DisplayName.AutoSize = true;
+            this.Label_DisplayName.Location = new System.Drawing.Point(3, 34);
+            this.Label_DisplayName.Name = "Label_DisplayName";
+            this.Label_DisplayName.Size = new System.Drawing.Size(99, 17);
+            this.Label_DisplayName.TabIndex = 16;
+            this.Label_DisplayName.Text = "Display Name:";
+            // 
+            // TextBox_Name
+            // 
+            this.TextBox_Name.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBox_Name.Location = new System.Drawing.Point(108, 3);
+            this.TextBox_Name.Name = "TextBox_Name";
+            this.TextBox_Name.Size = new System.Drawing.Size(239, 22);
+            this.TextBox_Name.TabIndex = 15;
+            // 
+            // Label_Name
+            // 
+            this.Label_Name.AutoSize = true;
+            this.Label_Name.Location = new System.Drawing.Point(3, 6);
+            this.Label_Name.Name = "Label_Name";
+            this.Label_Name.Size = new System.Drawing.Size(49, 17);
+            this.Label_Name.TabIndex = 14;
+            this.Label_Name.Text = "Name:";
+            // 
+            // PropertyGrid_Parameters
+            // 
+            this.PropertyGrid_Parameters.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PropertyGrid_Parameters.Location = new System.Drawing.Point(6, 171);
+            this.PropertyGrid_Parameters.Name = "PropertyGrid_Parameters";
+            this.PropertyGrid_Parameters.Size = new System.Drawing.Size(341, 276);
+            this.PropertyGrid_Parameters.TabIndex = 13;
+            // 
+            // MaterialControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.TextBox_Density);
+            this.Controls.Add(this.Label_Density);
+            this.Controls.Add(this.TextBox_SpecificHeatCapacity);
+            this.Controls.Add(this.Label_SpecificHeatCapacity);
+            this.Controls.Add(this.TextBox_ThermalConductivity);
+            this.Controls.Add(this.Label_ThermalConductivity);
+            this.Controls.Add(this.TextBox_Description);
+            this.Controls.Add(this.Label_Description);
+            this.Controls.Add(this.TextBox_DisplayName);
+            this.Controls.Add(this.Label_DisplayName);
+            this.Controls.Add(this.TextBox_Name);
+            this.Controls.Add(this.Label_Name);
+            this.Controls.Add(this.PropertyGrid_Parameters);
+            this.Name = "MaterialControl";
+            this.Size = new System.Drawing.Size(350, 450);
+            this.Load += new System.EventHandler(this.MaterialControl_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox_Density;
+        private System.Windows.Forms.Label Label_Density;
+        private System.Windows.Forms.TextBox TextBox_SpecificHeatCapacity;
+        private System.Windows.Forms.Label Label_SpecificHeatCapacity;
+        private System.Windows.Forms.TextBox TextBox_ThermalConductivity;
+        private System.Windows.Forms.Label Label_ThermalConductivity;
+        private System.Windows.Forms.TextBox TextBox_Description;
+        private System.Windows.Forms.Label Label_Description;
+        private System.Windows.Forms.TextBox TextBox_DisplayName;
+        private System.Windows.Forms.Label Label_DisplayName;
+        private System.Windows.Forms.TextBox TextBox_Name;
+        private System.Windows.Forms.Label Label_Name;
+        private System.Windows.Forms.PropertyGrid PropertyGrid_Parameters;
+    }
+}
--- SAM_Windows/SAM.Core.Windows/Forms/MaterialForm.cs
+++ SAM_Windows/SAM.Core.Windows/Forms/MaterialForm.cs
@@ -7,9 +7,6 @@ namespace SAM.Core.Windows.Forms
 {
     public partial class MaterialForm : Form
     {
-        private IMaterial material;
-        private HashSet<Enum> enums;
-
         public MaterialForm()
         {
             InitializeComponent();
@@ -19,152 +16,26 @@ namespace SAM.Core.Windows.Forms
         {
             InitializeComponent();
 
-            this.material = material;
-            if(this.material == null)
+            IMaterial material_Temp = material;
+            if (material == null)
             {
-                using (ComboBoxForm<MaterialType> comboBoxForm = new ComboBoxForm<MaterialType>("Select Material Type", Enum.GetValues(typeof(MaterialType)).Cast<MaterialType>().ToList().FindAll(x => x != MaterialType.Undefined)))
-                {
-                    DialogResult dialogResult = comboBoxForm.ShowDialog(this);
-                    if (dialogResult != DialogResult.OK)
-                    {
-                        DialogResult = dialogResult;
-                        Close();
-                    }
-
-                    switch(comboBoxForm.SelectedItem)
-                    {
-                        case MaterialType.Gas:
-                            this.material = new GasMaterial(Guid.NewGuid(), "Gas Material", "Gas Material", null, 0, 0, 0, 0);
-                            break;
-
-                        case MaterialType.Opaque:
-                            this.material = new OpaqueMaterial("Opaque Material", null, "Opaque Material", null, 0, 0, 0);
-                            break;
-
-                        case MaterialType.Transparent:
-                            this.material = new TransparentMaterial("Opaque Material", null, "Opaque Material", null, 0, 0, 0);
-                            break;
-                    }
-                }
+                material_Temp = Create.Material(this);
             }
 
-            if(enums != null)
-            {
-                this.enums = new HashSet<Enum>();
-                foreach(Enum @enum in enums)
-                {
-                    this.enums.Add(@enum);
-                }
-            }
+            MaterialControl_Main.Material = material_Temp;
+            MaterialControl_Main.Enums = enums?.ToList();
         }
 
         private void MaterialForm_Load(object sender, EventArgs e)
         {
-            PropertyGrid_Parameters.HidePropertyPages();
 
-            if (material != null)
-            {
-                TextBox_Name.Text = material?.Name;
-                Material material_Temp = material as Material;
-                if(material_Temp != null)
-                {
-                    CustomParameters customParameters = Create.CustomParameters(material_Temp, enums?.ToArray());
-
-                    TextBox_DisplayName.Text = material_Temp.DisplayName;
-                    TextBox_Description.Text = material_Temp.Description;
-
-                    string value;
-
-                    value = double.IsNaN(material_Temp.ThermalConductivity) ? null : material_Temp.ThermalConductivity.ToString();
-                    TextBox_ThermalConductivity.Text = value;
-
-                    value = double.IsNaN(material_Temp.SpecificHeatCapacity) ? null : material_Temp.SpecificHeatCapacity.ToString();
-                    TextBox_SpecificHeatCapacity.Text = value;
-
-                    value = double.IsNaN(material_Temp.Density) ? null : material_Temp.Density.ToString();
-                    TextBox_Density.Text = value;
-
-                    CustomParameter customParameter = null;
-                    if (material_Temp is FluidMaterial)
-                    {
-                        FluidMaterial fluidMaterial = (FluidMaterial)material_Temp;
-                        customParameter = new CustomParameter("Dynamic Viscosity", "Dynamic Viscosity of Fluid [kg/(m*s)]", AccessType.ReadWrite, new Attributes.DoubleParameterValue(0), typeof(FluidMaterial).Assembly.Name(), fluidMaterial.DynamicViscosity);
-
-                        customParameters?.Add(customParameter);
-                    }
-
-                    PropertyGrid_Parameters.SelectedObject = customParameters;
-                }
-            }
-
-            TextBox_ThermalConductivity.KeyPress += new KeyPressEventHandler(EventHandler.ControlText_NumberOnly);
-            TextBox_SpecificHeatCapacity.KeyPress += new KeyPressEventHandler(EventHandler.ControlText_NumberOnly);
-            TextBox_Density.KeyPress += new KeyPressEventHandler(EventHandler.ControlText_NumberOnly);
         }
 
         public IMaterial Material
         {
             get
             {
-                if(material == null)
-                {
-                    return null;
-                }
-
-                string name = TextBox_Name.Text;
-                string displayName = TextBox_DisplayName.Text;
-                string description = TextBox_Description.Text;
-                if(!Core.Query.TryConvert(TextBox_ThermalConductivity.Text, out double thermalConductivity))
-                {
-                    thermalConductivity = double.NaN;
-                }
-
-                if (!Core.Query.TryConvert(TextBox_SpecificHeatCapacity.Text, out double specificHeatCapacity))
-                {
-                    specificHeatCapacity = double.NaN;
-                }
-
-                if (!Core.Query.TryConvert(TextBox_SpecificHeatCapacity.Text, out double density))
-                {
-                    density = double.NaN;
-                }
-
-                CustomParameters customParameters = PropertyGrid_Parameters.SelectedObject as CustomParameters;
-
-                IMaterial result = null;
-                switch(material.MaterialType())
-                {
-                    case MaterialType.Gas:
-                        
-                        CustomParameter customParameter = customParameters?.Cast<CustomParameter>().ToList().Find(x => x?.Name == "Dynamic Viscosity");
-                        if (!Core.Query.TryConvert(customParameter.Value, out double dynamicViscosity))
-                        {
-                            dynamicViscosity = double.NaN;
-                        }
-                        
-                        result = new GasMaterial(material.Guid, name, displayName, description, thermalConductivity, density, specificHeatCapacity, dynamicViscosity);
-                        break;
-
-                    case MaterialType.Opaque:
-                        Material opaqueMaterial = (OpaqueMaterial)material;
-                        result = new OpaqueMaterial(opaqueMaterial.Guid, name, displayName, description, thermalConductivity, density, specificHeatCapacity);
-                        break;
-
-                    case MaterialType.Transparent:
-                        Material transparentMaterial = (TransparentMaterial)material;
-                        result = new OpaqueMaterial(transparentMaterial.Guid, name, displayName, description, thermalConductivity, density, specificHeatCapacity);
-                        break;
-
-                    default:
-                        return null;
-                }
-
-                if(result is SAMObject)
-                {
-                    ((SAMObject)result).SetValues(customParameters);
-                }
-
-                return result;
+                return MaterialControl_Main.Material;
             }
         }
 
@@ -185,6 +56,8 @@ namespace SAM.Core.Windows.Forms
         private void MaterialForm_HelpButtonClicked(object sender, System.ComponentModel.CancelEventArgs e)
         {
             string link = "https://github.com/HoareLea/SAM/wiki/Construction#materials";
+
+            IMaterial material = Material;
             if (material != null)
             {
                 if(material is GasMaterial)
