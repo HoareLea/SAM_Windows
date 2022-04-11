11e351ead4d38325ce6bea2e49f96222fe9da940
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-04-11 15:55:58 +0200

    Fixed Parameter Added for BorderlessForm

--- SAM_Windows/SAM.Core.Windows/Forms/BorderlessForm.cs
+++ SAM_Windows/SAM.Core.Windows/Forms/BorderlessForm.cs
@@ -13,6 +13,8 @@ namespace SAM.Core.Windows.Forms
 {
     public partial class BorderlessForm : Form
     {
+        private bool @fixed;
+        
         [DllImportAttribute("user32.dll")]
         public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int LPAR);
         [DllImportAttribute("user32.dll")]
@@ -38,77 +40,95 @@ namespace SAM.Core.Windows.Forms
             }
         }
 
+        public bool Fixed
+        {
+            get
+            {
+                return @fixed;
+            }
+
+            set
+            {
+                @fixed = value;
+            }
+        }
+
         /// <summary>
         /// This gives us the ability to resize the borderless from any borders instead of just the lower right corner
         /// </summary>
         /// <param name="m">Message</param>
         protected override void WndProc(ref Message m)
         {
-            const int wmNcHitTest = 0x84;
-            const int htLeft = 10;
-            const int htRight = 11;
-            const int htTop = 12;
-            const int htTopLeft = 13;
-            const int htTopRight = 14;
-            const int htBottom = 15;
-            const int htBottomLeft = 16;
-            const int htBottomRight = 17;
-
-            if (m.Msg == wmNcHitTest)
+            if(!@fixed)
             {
-                int x = (int)(m.LParam.ToInt64() & 0xFFFF);
-                int y = (int)((m.LParam.ToInt64() & 0xFFFF0000) >> 16);
-                Point pt = PointToClient(new Point(x, y));
-                Size clientSize = ClientSize;
-                ///allow resize on the lower right corner
-                if (pt.X >= clientSize.Width - 16 && pt.Y >= clientSize.Height - 16 && clientSize.Height >= 16)
+                const int wmNcHitTest = 0x84;
+                const int htLeft = 10;
+                const int htRight = 11;
+                const int htTop = 12;
+                const int htTopLeft = 13;
+                const int htTopRight = 14;
+                const int htBottom = 15;
+                const int htBottomLeft = 16;
+                const int htBottomRight = 17;
+
+                if (m.Msg == wmNcHitTest)
                 {
-                    m.Result = (IntPtr)(IsMirrored ? htBottomLeft : htBottomRight);
-                    return;
-                }
-                ///allow resize on the lower left corner
-                if (pt.X <= 16 && pt.Y >= clientSize.Height - 16 && clientSize.Height >= 16)
-                {
-                    m.Result = (IntPtr)(IsMirrored ? htBottomRight : htBottomLeft);
-                    return;
-                }
-                ///allow resize on the upper right corner
-                if (pt.X <= 16 && pt.Y <= 16 && clientSize.Height >= 16)
-                {
-                    m.Result = (IntPtr)(IsMirrored ? htTopRight : htTopLeft);
-                    return;
-                }
-                ///allow resize on the upper left corner
-                if (pt.X >= clientSize.Width - 16 && pt.Y <= 16 && clientSize.Height >= 16)
-                {
-                    m.Result = (IntPtr)(IsMirrored ? htTopLeft : htTopRight);
-                    return;
-                }
-                ///allow resize on the top border
-                if (pt.Y <= 16 && clientSize.Height >= 16)
-                {
-                    m.Result = (IntPtr)(htTop);
-                    return;
-                }
-                ///allow resize on the bottom border
-                if (pt.Y >= clientSize.Height - 16 && clientSize.Height >= 16)
-                {
-                    m.Result = (IntPtr)(htBottom);
-                    return;
-                }
-                ///allow resize on the left border
-                if (pt.X <= 16 && clientSize.Height >= 16)
-                {
-                    m.Result = (IntPtr)(htLeft);
-                    return;
-                }
-                ///allow resize on the right border
-                if (pt.X >= clientSize.Width - 16 && clientSize.Height >= 16)
-                {
-                    m.Result = (IntPtr)(htRight);
-                    return;
+                    int x = (int)(m.LParam.ToInt64() & 0xFFFF);
+                    int y = (int)((m.LParam.ToInt64() & 0xFFFF0000) >> 16);
+                    Point pt = PointToClient(new Point(x, y));
+                    Size clientSize = ClientSize;
+                    ///allow resize on the lower right corner
+                    if (pt.X >= clientSize.Width - 16 && pt.Y >= clientSize.Height - 16 && clientSize.Height >= 16)
+                    {
+                        m.Result = (IntPtr)(IsMirrored ? htBottomLeft : htBottomRight);
+                        return;
+                    }
+                    ///allow resize on the lower left corner
+                    if (pt.X <= 16 && pt.Y >= clientSize.Height - 16 && clientSize.Height >= 16)
+                    {
+                        m.Result = (IntPtr)(IsMirrored ? htBottomRight : htBottomLeft);
+                        return;
+                    }
+                    ///allow resize on the upper right corner
+                    if (pt.X <= 16 && pt.Y <= 16 && clientSize.Height >= 16)
+                    {
+                        m.Result = (IntPtr)(IsMirrored ? htTopRight : htTopLeft);
+                        return;
+                    }
+                    ///allow resize on the upper left corner
+                    if (pt.X >= clientSize.Width - 16 && pt.Y <= 16 && clientSize.Height >= 16)
+                    {
+                        m.Result = (IntPtr)(IsMirrored ? htTopLeft : htTopRight);
+                        return;
+                    }
+                    ///allow resize on the top border
+                    if (pt.Y <= 16 && clientSize.Height >= 16)
+                    {
+                        m.Result = (IntPtr)(htTop);
+                        return;
+                    }
+                    ///allow resize on the bottom border
+                    if (pt.Y >= clientSize.Height - 16 && clientSize.Height >= 16)
+                    {
+                        m.Result = (IntPtr)(htBottom);
+                        return;
+                    }
+                    ///allow resize on the left border
+                    if (pt.X <= 16 && clientSize.Height >= 16)
+                    {
+                        m.Result = (IntPtr)(htLeft);
+                        return;
+                    }
+                    ///allow resize on the right border
+                    if (pt.X >= clientSize.Width - 16 && clientSize.Height >= 16)
+                    {
+                        m.Result = (IntPtr)(htRight);
+                        return;
+                    }
                 }
             }
+
+
             base.WndProc(ref m);
         }
 
