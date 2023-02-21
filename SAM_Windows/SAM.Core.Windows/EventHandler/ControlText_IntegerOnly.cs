618b31f4985d7675df2f557f7cd3b96a2843ed20
Ziolkowski <jakubziolkowski@digiproject.uk>
2023-02-21 09:10:05 +0000

    IntegerOnly EventHandler Implemented

--- SAM_Windows/SAM.Core.Windows/EventHandler/ControlText_IntegerOnly.cs
+++ SAM_Windows/SAM.Core.Windows/EventHandler/ControlText_IntegerOnly.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Forms;
+using System.Windows.Input;
 
 namespace SAM.Core.Windows
 {
@@ -7,6 +8,21 @@ namespace SAM.Core.Windows
         public static void ControlText_IntegerOnly(object sender, KeyPressEventArgs e)
         {
             e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        public static void ControlText_IntegerOnly(object sender, TextCompositionEventArgs e)
+        {
+            if(e.Text != null)
+            {
+                foreach(char @char in e.Text)
+                {
+                    if(!char.IsDigit(@char))
+                    {
+                        e.Handled = true;
+                        return;
+                    }
+                }
+            }
         }
 
     }
