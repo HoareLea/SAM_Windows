d03654ff410240ca4865408e050ce5beb437ad58
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-06-08 09:57:37 +0200

    Fix for Negative Values

--- SAM_Windows/SAM.Core.Windows/EventHandler/ControlText_NumberOnly.cs
+++ SAM_Windows/SAM.Core.Windows/EventHandler/ControlText_NumberOnly.cs
@@ -10,17 +10,29 @@ namespace SAM.Core.Windows
         public static void ControlText_NumberOnly(object sender, KeyPressEventArgs e)
         {
             string separator_String = CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+
             char separator = '.';
             if (!string.IsNullOrWhiteSpace(separator_String))
+            {
                 separator = separator_String.Trim()[0];
+            }
 
-            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != separator))
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != separator) && e.KeyChar != '-')
+            {
                 e.Handled = true;
+            }
 
             // only allow one decimal point
             string text = (sender as Control)?.Text;
             if ((e.KeyChar == separator) && !string.IsNullOrWhiteSpace(text) && (text.IndexOf(separator) > -1))
+            {
                 e.Handled = true;
+            }
+
+            if ((e.KeyChar == '-') && !string.IsNullOrWhiteSpace(text) && (text.IndexOf('-') > 0))
+            {
+                e.Handled = true;
+            }
         }
 
         public static void ControlText_NumberOnly(object sender, TextCompositionEventArgs e)
